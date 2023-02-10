7b1088fa86195e717d1f0b694a322d402cbbd076
Emanuel Suriano <emanuel.suriano@revolut.com>
2023-02-10 11:45:15 +0100

    fix: add base_url to all links and assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
+import { BASE_URL } from './src/shared';
+
+// https://astro.build/config
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://emasuriano.github.io',
-  base: '/demo-git-lfs-astro',
+  base: BASE_URL,
+  integrations: [image()],
 });
